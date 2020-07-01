ad3757e5828f3e92e8e739722cc3872fc5a718e9
Vitaly <vitaly.zubentsov@ya.ru>
2020-07-01 19:37:07 +0300

    Сделал эмуляцию ста соединенй сокет клиента к сокет серверу в потоках.

--- src/main/java/threads/socket/SocketClients.java
+++ src/main/java/threads/socket/SocketClients.java
@@ -3,38 +3,67 @@ package threads.socket;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.net.UnknownHostException;
 
-public class SocketClients implements AutoCloseable{
+public class SocketClients implements Runnable {
 
-    Socket socket;
+    InetAddress inetAddress;
+    int portNumber;
 
-    SocketClients(InetAddress inetAddress, int portNumber) throws IOException {
-        socket = new Socket(inetAddress, portNumber);
+    SocketClients (InetAddress inetAddress, int portNumber) {
+        this.inetAddress = inetAddress;
+        this.portNumber = portNumber;
+    }
+
+    public void connectToServer() throws InterruptedException, IOException {
+
+        try (Socket socketClient = new Socket(inetAddress, portNumber)) {
+
+            System.out.printf("Сокет клиент %s подключён\n", socketClient);
+
+            Thread.sleep(300000);
+
+            System.out.printf("Сокет клиент %s завершает подключение\n", socketClient);
+
+        }
     }
 
     @Override
-    public void close() throws Exception {
-        if (!socket.isClosed()) {
-           socket.close();
-       }
+    public void run() {
+
+        try {
+            connectToServer();
+        } catch (InterruptedException e) {
+            System.out.printf("Поток %s не смог корректно отработать wait\n", Thread.currentThread().getName());
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.out.printf("Поток %s не смог создать сокет соединение\n", Thread.currentThread().getName());
+            e.printStackTrace();
+        }
     }
 
-    public static void main (String[] args) throws Exception {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+
 
         int portNumber = 1076;
-        InetAddress inetAddress = InetAddress.getLocalHost();
-        try(SocketClients socketClient = new SocketClients(inetAddress, portNumber)) {
-
-            System.out.printf("Сокет клиент %s подключён",socketClient);
-
-            Thread.sleep(3000);
-
-        } catch (IOException e) {
-            System.out.println("Что то пошло не так, клиент не смог подключиться к серверу.");
+        InetAddress inetAddress;
+        try {
+            inetAddress = InetAddress.getLocalHost();
+        } catch (UnknownHostException e) {
+            System.out.println("Не удалось определить локальный сетевой адрес");
             e.printStackTrace();
             throw (e);
         }
 
+        for (int i=0; i<100; i++) {
+            SocketClients socketClient = new SocketClients(inetAddress, portNumber);
+            Thread thread = new Thread(socketClient);
+            thread.start();
+
+        }
 
     }
+
+
 }
