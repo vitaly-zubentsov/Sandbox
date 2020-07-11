bcb207c658f8a8699f3a7f362e41df2cb531f19f
Vitaly <vitaly.zubentsov@ya.ru>
2020-07-11 21:31:18 +0300

    Реализовал синхронизированный массив с использованием нескольких conditions для одного блока синхронизации (для интерфейса lock).

---
+++ src/main/java/threads/syncronizeArray/GetThread.java
@@ -0,0 +1,22 @@
+package threads.syncronizeArray;
+
+public class GetThread extends Thread {
+
+    SyncArray syncArray;
+
+    GetThread(SyncArray syncArray) {
+        this.syncArray = syncArray;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                syncArray.getObjectFromArray();
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
--- src/main/java/threads/syncronizeArray/SyncArray.java
+++ src/main/java/threads/syncronizeArray/SyncArray.java
@@ -5,20 +5,49 @@ import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class SyncArray {
-    Object[] syncArray = new Object[100];
+    Object[] syncArray = new Object[10];
 
     Lock lock = new ReentrantLock();
-    Condition writeCondition = lock.newCondition();
-    Condition readCondition = lock.newCondition();
-    int index, indexBefore, indexAfter;
+    Condition arrayIsFull = lock.newCondition();
+    Condition arrayIsEmpty = lock.newCondition();
+    int length, putIndex, getIndex;
 
 
-    public void putObjectInArray(Object object) {
-        //TODO
+    public void putObjectInArray(Object object) throws InterruptedException {
+        lock.lock();
+        try {
+            while (length == 10) {
+                arrayIsFull.await();
+            }
+            syncArray[putIndex] = object;
+            if (++putIndex == 10) {
+                putIndex = 0;
+            }
+            length++;
+            System.out.printf("Array contain - %d element(s)\n", length);
+            arrayIsEmpty.signal();
+        } finally {
+            lock.unlock();
+        }
+
     }
 
-    public void getObjectFromArray(Object object) {
-        //TODO
+    public Object getObjectFromArray() throws InterruptedException {
+        lock.lock();
+        try {
+            while (syncArray.length == 0) {
+                arrayIsEmpty.await();
+            }
+            if (++getIndex == 10) {
+                getIndex = 0;
+            }
+            length--;
+            System.out.printf("Array contain - %d element(s)\n", length);
+            arrayIsFull.signal();
+        } finally {
+            lock.unlock();
+        }
+        return  syncArray[getIndex];
     }
 
 }
