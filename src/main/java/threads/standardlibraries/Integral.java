14fbc3d9aa5fa566f0b7e9b8b558719c2f43fbd2
Vitaly <vitaly.zubentsov@ya.ru>
2020-07-15 22:10:59 +0300

    Сделана программа подсчета интеграла. Подсчёт осуществляется в нескольких потоках, потоки создаются вручную.

--- src/main/java/threads/standardlibraries/Integral.java
+++ src/main/java/threads/standardlibraries/Integral.java
@@ -1,27 +1,67 @@
 package threads.standardlibraries;
 
+import java.util.concurrent.atomic.DoubleAdder;
 import java.util.function.DoubleFunction;
 
 public class Integral {
 
-    public static final int STEPS = 10000000;
+    public static final int STEPS = 100000000;
 
-    public static double singleThread(DoubleFunction<Double> doubleFunction, double startPointOfRange, double endPointOfRange) {
-        return singleThread(doubleFunction, startPointOfRange, endPointOfRange, STEPS);
+    public static double calculatingInSingleThread(DoubleFunction<Double> doubleFunction, double startPointOfRange, double endPointOfRange) {
+
+        //обработка кореектности входных данных
+        if (startPointOfRange > endPointOfRange) {
+            return 0;
+        }
+
+        return calculatingInSingleThread(doubleFunction, startPointOfRange, endPointOfRange, STEPS);
     }
 
-    static double singleThread(DoubleFunction<Double> doubleFunction, double startPointOfRange, double endPointOfRange, int steps) {
+    static double calculatingInSingleThread(DoubleFunction<Double> doubleFunction, double startPointOfRange, double endPointOfRange, int steps) {
         double widthOfStep = (endPointOfRange - startPointOfRange) / steps;
         double sumOfArea = 0d;
 
         for (int i = 0; i < steps; i++) {
             double middlePointOfStep = startPointOfRange + widthOfStep * i + widthOfStep / 2;
             double valueOfFunctionAtPoint = doubleFunction.apply(middlePointOfStep);
-            sumOfArea += valueOfFunctionAtPoint*widthOfStep;
+            sumOfArea += valueOfFunctionAtPoint * widthOfStep;
         }
 
         return sumOfArea;
     }
 
-    //TODO сделать public static multiThread()
+    public static double calculatingInMultiThreadWithThreads(DoubleFunction<Double> doubleFunction, double startPointOfRange, double endPointOfRange, int countOfThreads) throws Exception {
+
+        //обработка кореектности входных данных
+        if (startPointOfRange > endPointOfRange) {
+            return 0;
+        }
+        if (countOfThreads == 0) {
+            throw new Exception("Count of threads can't equals to zero");
+        }
+        if (countOfThreads < 0) {
+            throw new Exception("Count of threads can't be negative number");
+        }
+
+
+        int countOfStepsForSubrange = STEPS / countOfThreads;
+        double widthOfSubrange = (endPointOfRange - startPointOfRange)/countOfThreads;
+
+        Thread[] threadsForCalculation = new Thread[countOfThreads];
+        final DoubleAdder sumOfArea = new DoubleAdder();
+        for (int i = 0; i < countOfThreads; i++) {
+
+            double startPointOfRangeForSubrange = startPointOfRange + i*widthOfSubrange;
+            double endPointOfRangeForSubrange= startPointOfRangeForSubrange+widthOfSubrange;
+
+            threadsForCalculation[i] = new Thread( () ->  sumOfArea.add(calculatingInSingleThread(doubleFunction, startPointOfRangeForSubrange, endPointOfRangeForSubrange, countOfStepsForSubrange)));
+            threadsForCalculation[i].start();
+        }
+
+        for (int i = 1; i<countOfThreads; i++) {
+            threadsForCalculation[i].join();
+        }
+        return sumOfArea.doubleValue();
+    }
 }
+
--- src/main/java/threads/standardlibraries/StandardLibraries.java
+++ src/main/java/threads/standardlibraries/StandardLibraries.java
@@ -2,17 +2,32 @@ package threads.standardlibraries;
 
 public class StandardLibraries {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
         //Расчет в одном потоке
         //несколько раз считаем для того, чтобы виртуальная машина оптимизировала код
-        Integral.singleThread(Math::sin, 0d, Math.PI/2);
-        Integral.singleThread(Math::sin, 0d, Math.PI/2);
+        Integral.calculatingInSingleThread(Math::sin, 0d, Math.PI/2);
+        Integral.calculatingInSingleThread(Math::sin, 0d, Math.PI/2);
         long timeOfStart = System.currentTimeMillis();
-        double areaOfFunction = Integral.singleThread(Math::sin, 0d, Math.PI/2);
+        double areaOfFunction = Integral.calculatingInSingleThread(Math::sin, 0d, Math.PI/2);
         long timeOfFinish = System.currentTimeMillis();
 
         System.out.printf("Single thread: %f Time: %d\n", areaOfFunction, timeOfFinish-timeOfStart);
+
+        //Определяем сколько процессоров в системе
+        final int COUNT_OF_PROCESSORS_IN_SYSTEM = Runtime.getRuntime().availableProcessors();
+
+        //Расчет в нескольких потоках(10) созданных вручную
+        //несколько раз считаем для того, чтобы виртуальная машина оптимизировала код
+        Integral.calculatingInSingleThread(Math::sin, 0d, Math.PI/2);
+        Integral.calculatingInSingleThread(Math::sin, 0d, Math.PI/2);
+        long timeOfStart2 = System.currentTimeMillis();
+        double areaOfFunction2 = Integral.calculatingInMultiThreadWithThreads(Math::sin, 0d, Math.PI/2, COUNT_OF_PROCESSORS_IN_SYSTEM);
+        long timeOfFinish2 = System.currentTimeMillis();
+
+        System.out.printf("Single thread: %f Time: %d\n", areaOfFunction2, timeOfFinish2-timeOfStart2);
+
+
     }
 
 }
