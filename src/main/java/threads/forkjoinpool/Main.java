0f155dfc6fbf48251142f918c4f7a4bfd0b9af84
Vitaly <vitaly.zubentsov@ya.ru>
2020-07-19 17:41:15 +0300

    Реализовал расчёт веса бинарного дерева с использованием recursive task

--- src/main/java/threads/forkjoinpool/Main.java
+++ src/main/java/threads/forkjoinpool/Main.java
@@ -1,12 +1,35 @@
 package threads.forkjoinpool;
 
+
 public class Main {
 
-    public static void main (String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException {
 
         Tree tree = new Tree();
-        System.out.println("Done!");
-        System.out.printf("Sum from constructor %d sum from calculate %d", tree.totalWeightOfTree, tree.calculateWeightOfTree(tree.rootOfTree));
 
+        //Расчет в одном потоке
+        //несколько раз считаем для того, чтобы виртуальная машина оптимизировала код
+        tree.calculateWeightOfTreeInSingleThread(tree.rootOfTree);
+        tree.calculateWeightOfTreeInSingleThread(tree.rootOfTree);
+
+        long timeOfStart = System.currentTimeMillis();
+        long weightOfTree = tree.calculateWeightOfTreeInSingleThread(tree.rootOfTree);
+        long timeOfFinish = System.currentTimeMillis();
+
+        System.out.printf("Calculating weight of tree in single thread: %d%n Time: %d\n", weightOfTree, timeOfFinish - timeOfStart);
+
+        //Расчет с использованием Recursive task
+        //несколько раз считаем для того, чтобы виртуальная машина оптимизировала код
+        tree.calculateWeightOfTreeInForkJoinPool(tree.rootOfTree);
+        tree.calculateWeightOfTreeInForkJoinPool(tree.rootOfTree);
+
+        long timeOfStart2 = System.currentTimeMillis();
+        long weightOfTree2 = tree.calculateWeightOfTreeInSingleThread(tree.rootOfTree);
+        long timeOfFinish2 = System.currentTimeMillis();
+
+        System.out.printf("Calculating weight of tree in recursive task(%d): %d%n Time: %d\n",Runtime.getRuntime().availableProcessors()*2, weightOfTree2, timeOfFinish2 - timeOfStart2);
+
+        //С увеличением оперативной памяти jvm и уменьшении глубины дерева увеличение производительности при распаралеливании растёт.
+        //При малом объеме ОЗУ jvm и максимальной глубине дерева разница в однопоточном выполнении и многопоточном на уровне погрешности.
     }
 }
--- src/main/java/threads/forkjoinpool/Tree.java
+++ src/main/java/threads/forkjoinpool/Tree.java
@@ -1,6 +1,8 @@
 package threads.forkjoinpool;
 
 import java.util.Random;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveTask;
 
 public class Tree {
 
@@ -9,12 +11,12 @@ public class Tree {
     TreeNode rootOfTree = new TreeNode();
 
     //Для глубины дерева равной двадцати семи, требуется выделить около 6.5Gb ОЗУ для jvm
-    private int deepOfTree = 27;
+    private int deepOfTree = 22;
     public long totalWeightOfTree;
 
 
     public Tree() {
-        createBranch(rootOfTree,this.deepOfTree);
+        createBranch(this.rootOfTree,this.deepOfTree);
     }
 
     /**
@@ -50,12 +52,67 @@ public class Tree {
      * @param node
      * @return
      */
-    public long calculateWeightOfTree(TreeNode node) {
+    public long calculateWeightOfTreeInSingleThread(TreeNode node) {
         //Вызываем рекурсивно функцию подсчета каждой ветви, до тех пор, пока не достигнем объекта, у которого поля
         // left и right будут null (то есть, до тех пор пока не достигнем ветви, не имеющей ответвлений)
        return (node.weight +
-               (node.left!=null?calculateWeightOfTree(node.left):0)+
-               (node.right!=null?calculateWeightOfTree(node.right):0));
+               (node.left!=null? calculateWeightOfTreeInSingleThread(node.left):0)+
+               (node.right!=null? calculateWeightOfTreeInSingleThread(node.right):0));
+    }
+
+    public long calculateWeightOfTreeInForkJoinPool(TreeNode node) {
+
+        class TaskForCalculatingWeightOfTree extends RecursiveTask<Long> {
+
+            private TreeNode node;
+            private int countOfThreads;
+            private long totalWeightOfTree;
+
+            TaskForCalculatingWeightOfTree(TreeNode rootOfTree, int countOfThreads) {
+                this.node = rootOfTree;
+                this.countOfThreads = countOfThreads;
+            }
+
+            @Override
+            protected Long compute() {
+                int newCountOfThreads = countOfThreads - 1;
+                //Если количество тредов закончилось, то продолжаем расчёт в одном потоке
+                if (countOfThreads<=0) {
+                    calculateWeightOfTreeInSingleThread(node);
+                }
+
+                this.totalWeightOfTree += node.weight;
+
+                //Если у ветви есть ответвления, то запускаем их расчет в отдельном потоке.
+                TaskForCalculatingWeightOfTree taskForCalculateRightNode = null, taskForCalculateLeftNode = null;
+                if (node.right!=null) {
+                    taskForCalculateRightNode = new TaskForCalculatingWeightOfTree(node.right,newCountOfThreads);
+                    taskForCalculateRightNode.fork();
+                }
+
+                if (node.left!=null) {
+                    taskForCalculateLeftNode = new TaskForCalculatingWeightOfTree(node.left,newCountOfThreads);
+                    taskForCalculateLeftNode.fork();
+                }
+
+                if (taskForCalculateRightNode!=null) {
+                    this.totalWeightOfTree+= taskForCalculateRightNode.join();
+                }
+
+                if (taskForCalculateLeftNode!=null) {
+                    this.totalWeightOfTree+= taskForCalculateLeftNode.join();
+                }
+
+                return this.totalWeightOfTree;
+            }
+
+        }
+
+        return ForkJoinPool.commonPool().invoke(
+                new TaskForCalculatingWeightOfTree(
+                        rootOfTree,
+                        Runtime.getRuntime().availableProcessors())
+        );
     }
 
 
