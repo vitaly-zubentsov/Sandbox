06f793dd53495d2b8fe57ea94c68851e868b032d
Vitaly <vitaly.zubentsov@ya.ru>
2020-08-18 21:41:24 +0300

    Добавлена возможность пользователю ввести подстроку по которой осуществляется поиск в столбце name, с последующим выводом совпадающих строк в консоль

--- data-base/src/DBselect.java
+++ data-base/src/DBselect.java
@@ -1,4 +1,5 @@
 import java.sql.*;
+import java.util.Scanner;
 
 public class DBselect {
 
@@ -9,12 +10,22 @@ public class DBselect {
 
 
 	public static void main(String[] args) throws ClassNotFoundException  {
-		//Нужно добавить драйвер базы данных (sandbox/lib)
+		//Для выполнения программы необходимо добавить драйвер базы данных (sandbox/lib)
+
+		//Загружаем драйвер
 		Class.forName(DRIVER_NAME);
+
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Input part of a course name to search: ");
+		String partOfSearchingCourse = sc.nextLine();
 
 		try (Connection conn = DriverManager.getConnection(CONNETION_STRING);) {
 			Statement cmd =  conn.createStatement();
-			String sql = "SELECT name, description, lenght FROM courses ORDER BY lenght";
+			//Выводим поля таблицы курсов при 
+			//совпадении параметра столбца name с подстрокой, введённой пользователем
+			String sql = "SELECT name, description, lenght FROM courses WHERE name LIKE '%" 
+					+ partOfSearchingCourse +"%' ORDER BY lenght";
 			ResultSet result = cmd.executeQuery(sql);
 			while (result.next())
 			{
@@ -23,6 +34,7 @@ public class DBselect {
 				int  lenght = result.getInt("lenght");
 				System.out.printf("%-10s : %-10s : %d \n",name, description,lenght);
 			}
+			result.close();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
