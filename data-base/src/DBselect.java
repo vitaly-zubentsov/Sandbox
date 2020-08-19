116bc4873b5f36f5779f9cafe0f819be674655f3
Vitaly <vitaly.zubentsov@ya.ru>
2020-08-19 22:50:36 +0300

    Реализовано выполнение хранимой в базе данных процедуры

--- data-base/src/DBselect.java
+++ data-base/src/DBselect.java
@@ -16,17 +16,25 @@ public class DBselect {
 		Class.forName(DRIVER_NAME);
 
 
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Input part of a course name to search: ");
-		String partOfSearchingCourse = sc.nextLine();
+
 
 		try (Connection conn = DriverManager.getConnection(CONNETION_STRING);) {
-			
-			//Подготавливаем запрос с последующей подстановкой текста введённого пользователем
+
+			CallableStatement storedProcedure = conn.prepareCall("call countCourses(?)");
+
+			storedProcedure.execute();
+			System.out.printf("Всего строк в таблице: %d\n",storedProcedure.getInt(1));
+
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Input part of a course name to search: ");
+			String partOfSearchingCourse = sc.nextLine();
+
+			//Подготавливаем параметризированный sql запрос
 			String sql = "SELECT name, description, lenght FROM courses WHERE name LIKE ? ORDER BY lenght";
 			PreparedStatement cmd = conn.prepareStatement(sql);
+			//Подставляем данный в параметризированный sql запрос
 			cmd.setString(1, "%" + partOfSearchingCourse + "%" );
-			
+
 			//Выводим поля таблицы курсов при 
 			//совпадении параметра столбца name с подстрокой, введённой пользователем
 			ResultSet result = cmd.executeQuery();
